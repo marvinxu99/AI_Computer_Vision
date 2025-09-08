171ddfdf46479e3097913cfa33a83a27e5236700
Marvin Xu <marvinxu99@hotmail.com>
2025-09-07 21:13:13 -0700

    ...

--- learn/tensors_1.py
+++ learn/tensors_1.py
@@ -1,3 +1,5 @@
+# https://docs.pytorch.org/tutorials/beginner/blitz/tensor_tutorial.html
+
 import torch
 import numpy as np
 
@@ -25,3 +27,11 @@ zeros_tensor = torch.zeros(shape)
 print(f"Random Tensor: \n {rand_tensor} \n")
 print(f"Ones Tensor: \n {ones_tensor} \n")
 print(f"Zeros Tensor: \n {zeros_tensor}")
+
+# Tensor Attributes
+# Tensor attributes describe their shape, datatype, and the device on which they are stored.
+tensor = torch.rand(3, 4)
+
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Device tensor is stored on: {tensor.device}")
---
+++ learn/tensors_2 ops.py
@@ -0,0 +1,28 @@
+# https://docs.pytorch.org/tutorials/beginner/blitz/tensor_tutorial.html
+
+import torch
+import numpy as np
+
+tensor = torch.ones(4, 4)
+tensor[:,1] = 0
+print(tensor)
+
+# We move our tensor to the GPU if available
+if torch.cuda.is_available():
+  tensor = tensor.to('cuda')
+  print(f"Device tensor is stored on: {tensor.device}")
+else:
+  print("cuda is not available.")
+
+
+# Joining tensors 
+# You can use torch.cat to concatenate a sequence of tensors along a given dimension. 
+# See also torch.stack, another tensor joining op that is subtly different from torch.cat.
+t1 = torch.cat([tensor, tensor, tensor], dim=1)
+print(t1)
+
+# Multiplying tensors
+# This computes the element-wise product
+print(f"tensor.mul(tensor) \n {tensor.mul(tensor)} \n")
+# Alternative syntax:
+print(f"tensor * tensor \n {tensor * tensor}")
